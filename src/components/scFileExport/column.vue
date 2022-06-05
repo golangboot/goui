730aaa1fa0fea9b35031e34b2be31e5ed5d67da9
sakuya <81883387@qq.com>
2022-06-05 10:42:37 +0800

    FIX scFileExport 文件导出列设置宽度溢出

--- src/components/scFileExport/column.vue
+++ src/components/scFileExport/column.vue
@@ -1,7 +1,7 @@
 
 <template>
 	<el-table ref="table" :data="columnData" row-key="prop" style="width: 100%" border>
-	    <el-table-column prop="" label="排序" width="58">
+	    <el-table-column prop="" label="排序" width="60">
 			<el-tag class="move" style="cursor: move;"><el-icon style="cursor: move;"><el-icon-d-caret/></el-icon></el-tag>
 		</el-table-column>
 	    <el-table-column prop="label" label="列名">
